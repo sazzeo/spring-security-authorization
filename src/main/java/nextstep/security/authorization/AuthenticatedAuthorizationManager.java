820e7081fc32c38bb1a26011b7f1f42c5fb7bc2d
jiyeong-dt <imjee02@gmail.com>
2025-02-11 00:31:04 +0900

    feat: AuthorityAuthorizationManager 추가

---
+++ src/main/java/nextstep/security/authorization/AuthenticatedAuthorizationManager.java
@@ -0,0 +1,14 @@
+package nextstep.security.authorization;
+
+import nextstep.security.authentication.Authentication;
+import nextstep.security.authentication.AuthenticationException;
+
+public class AuthenticatedAuthorizationManager implements AuthorizationManager<Object> {
+    @Override
+    public AuthorizationDecision check(final Authentication authentication, final Object object) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new AuthenticationException();
+        }
+        return AuthorizationDecision.success();
+    }
+}
