9c7ebba329fb4ce162be71435fdf9f934544eca4
jiyeong-dt <imjee02@gmail.com>
2025-02-11 00:21:33 +0900

    fix: RequestMatcherEntry record로 변경

--- src/main/java/nextstep/security/requestmatcher/RequestMatcherEntry.java
+++ src/main/java/nextstep/security/requestmatcher/RequestMatcherEntry.java
@@ -1,12 +1,4 @@
 package nextstep.security.requestmatcher;
 
-public class RequestMatcherEntry<T> {
-    private final RequestMatcher requestMatcher;
-    private final T entry;
-
-    public RequestMatcherEntry(final RequestMatcher requestMatcher, final T entry) {
-        this.requestMatcher = requestMatcher;
-        this.entry = entry;
-    }
-
+public record RequestMatcherEntry<T>(RequestMatcher requestMatcher, T entry) {
 }
