79da0898ee0da1fdd7a86e74a05de47e9f40636d
jiyeong-dt <imjee02@gmail.com>
2025-02-26 00:08:42 +0900

    feat: RequestAuthorizationAuthoritiesContext 관련 부분 삭제

--- src/main/java/nextstep/security/authorization/RequestAuthorizationContext.java
+++
@@ -1,9 +0,0 @@
-package nextstep.security.authorization;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-public interface RequestAuthorizationContext<T> {
-    HttpServletRequest getRequest();
-
-    T getOthers();
-}
